ce8d427a214a93cbf310d9e01704f3b48285b3c2
Darwin Schweitzer <darsch@microsoft.com>
2018-01-09 10:10:00 -0800

    pip install

--- iotedge/Extensions/IoTEdge/install.sh
+++ iotedge/Extensions/IoTEdge/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install IOT Edge Runtime and register the edge device
 source /anaconda/bin/activate root
+apt-get install python-pip
 pip install -U azure-iot-edge-runtime-ctl
 iotedgectl setup --connection-string "$1" --auto-cert-gen-force-no-passwords
 iotedgectl start
